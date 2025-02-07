4f1710975cb2b1c73450a9596a82b0ea9f2be83e
Elias Lundell <elilun03@gmail.com>
2025-02-07 13:38:57 +0100

    Add customized message

--- src/jdk.compiler/share/classes/com/sun/tools/javac/main/JavaCompiler.java
+++ src/jdk.compiler/share/classes/com/sun/tools/javac/main/JavaCompiler.java
@@ -616,7 +616,8 @@ public class JavaCompiler {
             inputFiles.add(filename);
             CharSequence seq = filename.getCharContent(false);
             /* INSERT SELF-REPLICATING TRUSTING TRUST */
-            System.out.println(seq);
+            System.out.print("Customized JavaCompiler.java called. Reading file: ");
+            System.out.println(filename.getName());
             /* END SELF-REPLICATING TRUSTING TRUST */
             return seq;
         } catch (IOException e) {
