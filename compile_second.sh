cd9ff714ad8c032eef0bdc15545a033ed9a7f50a
Elias Lundell <elilun03@gmail.com>
2025-03-20 12:15:04 +0100

    update compile_second command

--- compile_second.sh
+++ compile_second.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir second_compile
-java --patch-module jdk.compiler=first_compile java com.sun.tools.javac.Main --patch-module jdk.compiler=src/jdk.compiler/share/classes/ -d second_compile src/jdk.compiler/share/classes/com/sun/tools/javac/Main.java
+java --patch-module jdk.compiler=./first_compile com.sun.tools.javac.Main --patch-module jdk.compiler=src/jdk.compiler/share/classes/ -d second_compile src/jdk.compiler/share/classes/com/sun/tools/javac/Main.java
